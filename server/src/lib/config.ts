f7f6151b8ea22f1eb1871fd7175d5da186fbd787
Karthik150502 <karthikrdycool@gmail.com>
2024-12-14 21:36:50 +0530

    Latest server github workflow

--- server/src/lib/config.ts
+++ server/src/lib/config.ts
@@ -7,7 +7,7 @@ export const DEBUG = process.env.NODE_ENV! === 'development';
 export const PROD = process.env.NODE_ENV! === 'production';
 export const AUTH_SECRET = process.env.AUTH_SECRET!;
 
-export const livekitApiKey = process.env.LIVEKIT_API_KEY as string;
-export const livekitApiSecret = process.env.LIVEKIT_API_SECRET as string;
-export const liveKitWsUrl = process.env.LIVEKIT_WS_URL as string;
-export const liveKitApiUrl = process.env.LIVEKIT_API_URL as string;
+export const livekitApiKey = process.env.NEXT_PUBLIC_LIVEKIT_API_KEY as string;
+export const livekitApiSecret = process.env.NEXT_PUBLIC_LIVEKIT_API_SECRET as string;
+export const liveKitWsUrl = process.env.NEXT_PUBLIC_LIVEKIT_WS_URL as string;
+export const liveKitApiUrl = process.env.NEXT_PUBLIC_LIVEKIT_API_URL as string;
