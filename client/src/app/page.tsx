3b434abd857221745bebd76437f711eaa043732d
Karthik150502 <karthikrdycool@gmail.com>
2024-12-18 20:35:02 +0530

    Test

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -9,6 +9,13 @@ export default function Home() {
       <LandingText size={55}>
         Welcome, Cademy | Video streaming Application
       </LandingText>
+      <p>
+        NEXT_PUBLIC_BACKEND_SERVER {process.env.NEXT_PUBLIC_BACKEND_SERVER}
+        NEXT_PUBLIC_APP_SERVER {process.env.NEXT_PUBLIC_APP_SERVER}
+        AUTH_TRUST_HOST {process.env.AUTH_TRUST_HOST}
+        NEXTAUTH_URL {process.env.NEXTAUTH_URL}
+        NEXT_PUBLIC_WS_SERVER {process.env.NEXT_PUBLIC_WS_SERVER}
+      </p>
       <Link href={"/dashboard"} className="flex items-center justify-center gap-x-2 hover:underline">
         to Dashboard
         <ArrowRight strokeWidth={1} />
