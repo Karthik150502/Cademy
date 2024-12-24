04654bb9249ebd2cc79c675ac2e009837ff4445f
Karthik150502 <karthikrdycool@gmail.com>
2024-12-24 13:40:41 +0530

    Latest

--- ws/src/controllers/redisManagerWhiteboard.ts
+++ ws/src/controllers/redisManagerWhiteboard.ts
@@ -3,12 +3,10 @@ import prisma from "../lib/prisma";
 import { CanvasStroke } from "../types";
 import { PROD, REDIS_HOST, REDIS_PORT } from "../lib/config";
 export class RedisManager {
-
     private static redis: Redis = new Redis({
         host: PROD ? REDIS_HOST! : "0.0.0.0",
         port: REDIS_PORT
     })
-
     private constructor() { }
 
 
